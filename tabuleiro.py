5e465ba0d936a2ac98c84d49fd7df7f1d04e0e85
dougcunha <dougcunha@gmail.com>
2011-05-08 20:10:24 -0300

    tabuleiro percorrendo as celulas com docteste

--- tabuleiro.py
+++ tabuleiro.py
@@ -6,25 +6,151 @@
 import numpy
 
 class Tabuleiro:
-    celulas = {}
+    celulas = []
+    acima = 1
+    abaixo = 2
+    esquerda = 3
+    direita = 4
+    #__init__
     def __init__(self):
        pass
     
+    #tocar
     def tocar(self, linha, coluna):
-        self.celulas[linha, coluna] += 1
+        '''
+            >>> tab = Tabuleiro()
+            >>> tab.iniciar([[1,2,1,3,4],[0,3,4,2,3],[4,3,4,2,3],[3,4,3,2,1],[2,3,4,3,4],[2,3,4,3,4]])
+            >>> tab.tocar(0, 0)
+            >>> tab.celulas[0][0]
+            2
+            >>> tab.tocar(0, 0)
+            >>> tab.celulas[0][0]
+            3
+            >>> tab.tocar(1, 2)
+            >>> tab.celulas[1][2]
+            0
+        '''
+        if not self.celulaEhValida(linha, coluna):
+            return
+        self.celulas[linha][coluna] += 1
         self.percorrer()
 
+    #celulaEhValida
+    def celulaEhValida(self, linha, coluna):
+        '''
+            >>> tab = Tabuleiro()
+            >>> tab.iniciar([[1,2,1,3,4],[0,3,4,2,3],[4,3,4,2,3],[3,4,3,2,1],[2,3,4,3,4],[2,3,4,3,4]])
+            >>> tab.celulaEhValida(0, 0)
+            True
+            >>> tab.celulaEhValida(1, 0)
+            True
+            >>> tab.celulaEhValida(-1, 0)
+            False
+        '''
+        return (coluna >= 0) and (coluna < self.colunas()) and \
+        (linha >= 0) and (linha < self.linhas())
+
+    #celula
+    def celula(self, linha, coluna):
+        return self.celulas[linha][coluna]
+        
+    #obterCelulaNaoVazia
+    def obterCelulaNaoVazia(self, linha, coluna, direcao):
+        '''
+            >>> tab = Tabuleiro()
+            >>> tab.iniciar([[1,2,1,3,4],[0,3,4,2,3],[4,3,4,2,3],[3,4,3,2,1],[2,3,4,3,4],[2,3,4,3,4]])
+            >>> tab.obterCelulaNaoVazia(0, 0, tab.acima)
+            (0, 0)
+            >>> tab.obterCelulaNaoVazia(1, 0, tab.acima)
+            (0, 0)
+            >>> tab.celula(2, 0)
+            4
+            >>> tab.obterCelulaNaoVazia(2, 0, tab.acima)
+            (2, 0)
+            >>> tab.obterCelulaNaoVazia(1, 1, tab.acima)
+            (1, 1)
+        '''
+        if not self.celulaEhValida(linha, coluna):
+            return -1, -1            
+        if self.celula(linha,coluna) in range(1, 5):
+            return linha, coluna
+        if direcao == self.acima:
+            return self.obterCelulaNaoVazia(linha - 1, coluna, direcao)
+        elif direcao == self.abaixo:
+            return self.obterCelulaNaoVazia(linha + 1, coluna, direcao)
+        elif direcao == self.esquerda:
+            return self.obterCelulaNaoVazia(linha, coluna - 1, direcao)
+        else:
+            return self.obterCelulaNaoVazia(linha, coluna + 1, direcao)
+        
+
+    #iniciar
     def iniciar(self, matriz):
         self.celulas = matriz
 
+    #percorrer
     def percorrer(self):
-        pass
+        """
+            >>> tab = Tabuleiro()
+            >>> tab.iniciar([[5,2,1,3,4],[0,3,4,2,3],[4,3,4,2,3],[3,4,3,2,1],[2,3,4,3,4],[2,3,4,3,4]])
+            >>> tab.percorrer()
+            >>> tab.celulas[0][0]
+            0
+            >>> tab.celulas[0][1]
+            3
+            >>> tab.celulas[1][0]
+            0
+            >>> tab.celulas[2][0]
+            0
+            >>> tab.celulas[3][0]
+            4
+        """
+        for linha in range(0, self.linhas() - 1):
+            for coluna in range(0, self.colunas() - 1):
+                if self.celula(linha, coluna) == 5:
+                    self.celulas[linha][coluna] = 0
+                    
+                    x, y = self.obterCelulaNaoVazia(linha - 1, coluna, self.acima)
+                    self.tocar(x, y)
+                    
+                    x, y = self.obterCelulaNaoVazia(linha + 1, coluna, self.abaixo)
+                    self.tocar(x, y)
+                    
+                    x, y = self.obterCelulaNaoVazia(linha, coluna - 1, self.esquerda)
+                    self.tocar(x, y)
+                    
+                    x, y = self.obterCelulaNaoVazia(linha, coluna + 1, self.direita)
+                    self.tocar(x, y)
+
+    #linhas
+    def linhas(self):
+        return len(self.celulas)
+    
+    #colunas
+    def colunas(self):
+        if self.linhas == 0: return 0
+        return len(self.celulas[0])
+    
+    #imprimir
+    def imprimir(self, linha, coluna):
+        print self.celulas[linha][coluna]
+
+    #__repr__
+    def __repr__(self):
+        return "Tabuleiro de %d linhas e %d colunas\n %s" % (self.linhas(), self.colunas(), \
+                                                          [linha for linha in self.celulas])
+                                                        
 
 if __name__ == '__main__':
-    tab = Tabuleiro()
-    tab.iniciar({(1,1):1, (2,3): 3})
-    print tab.celulas
-    print tab.celulas[1,1]
-    tab.tocar(1, 1)
-    print tab.celulas[1,1]
+    '''tab = Tabuleiro()
+    tab.iniciar([
+        [1,2,1,3,4],
+        [0,3,4,2,3],
+        [4,3,4,2,3],
+        [3,4,3,2,1],
+        [2,3,4,3,4],
+        [2,3,4,3,4]]) '''
+    import doctest
+    doctest.testmod()
+    
 	
