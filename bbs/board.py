d586851939f7349415e7f775d0157baaaeb7c694
unknown <fernando.macedo@.esys3.com.br>
2011-05-12 13:23:18 -0300

    Revision of board score calcs.

--- bbs/board.py
+++ bbs/board.py
@@ -2,11 +2,19 @@
 
 def scoreboard(board):
     """
-    >>> s = scoreboard({(0,1): 4, (0,2): 3, (1,2): 5})
+    Calculate value that represents how much longer a board is to be solved.
+    Board solved returns 0.
+        4 -> 1
+        3 -> 2
+        2 -> 3
+        1 -> 4
+        0 -> 0
+    >>> s = scoreboard({(0,1): 4, (0,2): 3, (1,2): 1})
     >>> s
-    12
+    7
     """
-    return sum(board.values())
+    vl = lambda x: (5-x) %5
+    return sum([vl(x) for x in board.values()])
 
 def newboard(game):
     row = 0
